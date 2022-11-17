cdbd471fbe9e12609ec88ae9d2ccf1996b17ebb8
Antonio <antonio.marinovvvv@gmail.com>
2022-11-17 18:25:04 +0200

    added planetrepository

--- PlanetWars/Repositories/Entities/PlanetRepository.cs
+++ PlanetWars/Repositories/Entities/PlanetRepository.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PlanetWars.Repositories.Entities
 {
-    internal class Class1
+    public class PlanetRepository : IRepository<IPlanet>
     {
+        private List<IPlanet> planets;
+
+        public PlanetRepository()
+        {
+            this.planets = new List<IPlanet>();
+        }
+        public IReadOnlyCollection<IPlanet> Models
+        { get { return this.planets; } 
+        }
+
+        public void AddItem(IPlanet model)
+        {
+            this.planets.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            return this.planets.FirstOrDefault(o => o.GetType().Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            return this.planets.Remove(this.FindByName(name));
+        }
     }
 }
