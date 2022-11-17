ccb59b8ec82d8bc2b3d15b52578fe8577d528d04
Antonio <antonio.marinovvvv@gmail.com>
2022-11-17 23:35:32 +0200

    planet logic

--- PlanetWars/Models/Planets/Entities/Planet.cs
+++ PlanetWars/Models/Planets/Entities/Planet.cs
@@ -1,32 +1,118 @@
 ﻿using PlanetWars.Models.MilitaryUnits.Contracts;
 using PlanetWars.Models.Planets.Contracts;
 using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
+using PlanetWars.Repositories.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 
 namespace PlanetWars.Models.Planets.Entities
 {
     public abstract class Planet : IPlanet
     {
-        public string Name => throw new NotImplementedException();
+        private string name;
+        private double budget;
+        private double militaryPower;
+        private WeaponRepository weaponRepository;
+        private UnitRepository unitRepository;
 
-        public double Budget => throw new NotImplementedException();
+        public Planet (string name, double budget)
+        {
+            this.weaponRepository = new WeaponRepository();
+            this.unitRepository = new UnitRepository();
 
-        public double MilitaryPower => throw new NotImplementedException();
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(Utilities.Messages.ExceptionMessages.InvalidPlanetName);
+            }
 
-        public IReadOnlyCollection<IMilitaryUnit> Army => throw new NotImplementedException();
+            this.name = name;
 
-        public IReadOnlyCollection<IWeapon> Weapons => throw new NotImplementedException();
+            if (budget < 0)
+            {
+                throw new ArgumentException(Utilities.Messages.ExceptionMessages.InvalidBudgetAmount);
+            }
+
+            this.budget = budget;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public double Budget {
+            get
+            {
+                return this.budget;
+            }
+            private set
+            {
+                this.budget = value;
+            }
+        }
+
+        public double MilitaryPower
+        {
+            get
+            {
+                return this.militaryPower;
+            }
+
+            private set
+            {
+                this.militaryPower = CalculateMilitaryPower(this.weaponRepository, this.unitRepository);
+            }
+        }
+
+        private double CalculateMilitaryPower(WeaponRepository weapons, UnitRepository army)
+        {
+            double sum = weapons.Models.Sum(o => o.DestructionLevel) +
+                                army.Models.Sum(o => o.EnduranceLevel);
+
+            if (army.Models.Any(o => o.GetType().Name == "AnonymousImpactUnit")) ;
+            {
+                sum = (sum * 0.3) + sum;
+            }
+
+            if (weapons.Models.Any(o => o.GetType().Name == "NuclearWeapon "))
+            {
+                sum = (sum * 0.45) + sum;
+            }
+
+            return Math.Round(sum, 3);
+        }
+
+        public IReadOnlyCollection<IMilitaryUnit> Army
+        {
+            get
+            {
+                return this.unitRepository.Models;
+            }
+        }
+
+        public IReadOnlyCollection<IWeapon> Weapons
+        {
+            get
+            {
+                return this.weaponRepository.Models;
+            }
+        }
 
         public void AddUnit(IMilitaryUnit unit)
         {
-            throw new NotImplementedException();
+            this.unitRepository.AddItem(unit);
         }
 
         public void AddWeapon(IWeapon weapon)
         {
-            throw new NotImplementedException();
+            this.weaponRepository.AddItem(weapon);
         }
 
         public string PlanetInfo()
@@ -41,12 +127,22 @@ namespace PlanetWars.Models.Planets.Entities
 
         public void Spend(double amount)
         {
-            throw new NotImplementedException();
+            if (amount > this.Budget)
+            {
+                throw new InvalidOperationException(Utilities.Messages.ExceptionMessages.UnsufficientBudget);
+            }
+            else
+            {
+                this.Budget -= amount;
+            }
         }
 
         public void TrainArmy()
         {
-            throw new NotImplementedException();
+            foreach (var item in this.Army)
+            {
+                item.IncreaseEndurance();
+            }
         }
     }
 }
