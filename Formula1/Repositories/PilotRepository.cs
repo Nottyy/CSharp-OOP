94f98be8c37202893f3f5687436075cc1041090d
Antonio <antonio.marinovvvv@gmail.com>
2022-11-23 23:40:42 +0200

    racerepository and pilotrepository added

---
+++ Formula1/Repositories/PilotRepository.cs
@@ -0,0 +1,43 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1.Repositories
+{
+    internal class PilotRepository : IRepository<IPilot>
+    {
+        private ICollection<IPilot> pilots;
+
+        public PilotRepository()
+        {
+            this.pilots = new List<IPilot>();
+        }
+
+        public IReadOnlyCollection<IPilot> Models
+        {
+            get
+            {
+                return (IReadOnlyCollection<IPilot>)this.pilots;
+            }
+        }
+
+        public void Add(IPilot model)
+        {
+            this.pilots.Add(model);
+        }
+
+        public IPilot FindByName(string name)
+        {
+            return this.pilots.FirstOrDefault(o => o.FullName == name);
+        }
+
+        public bool Remove(IPilot model)
+        {
+            return this.pilots.Remove(model);
+        }
+    }
+}
---
+++ Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,43 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1.Repositories
+{
+    internal class RaceRepository : IRepository<IRace>
+    {
+        private ICollection<IRace> races;
+
+        public RaceRepository()
+        {
+            this.races = new List<IRace>();
+        }
+        public IReadOnlyCollection<IRace> Models
+        {
+            get
+            {
+                return (IReadOnlyCollection<IRace>)this.races;
+            }
+        }
+
+        public void Add(IRace model)
+        {
+            this.races.Add(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            return this.races.FirstOrDefault(o => o.RaceName == name);
+        }
+
+        public bool Remove(IRace model)
+        {
+            return this.races.Remove(model);
+        }
+    }
+}
