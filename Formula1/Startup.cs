19019255dbb19a54266c53ce7636e381a2ce80e0
Antonio <antonio.marinovvvv@gmail.com>
2022-11-23 20:31:45 +0200

    Formula1 OOP EXAM task start

---
+++ Formula1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Formula1.Core;
+using Formula1.Core.Contracts;
+
+namespace Formula1
+{
+    internal class Startup
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
