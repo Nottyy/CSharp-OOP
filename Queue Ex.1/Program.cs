67bc72ed1b24faa2a144ff662003a73eaaf316e2
Antonio <antonio.marinovvvv@gmail.com>
2023-01-02 22:46:10 +0200

    done

---
+++ Queue Ex.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Ex._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sequence N -> N + 1 | N * 2 //// Find the first index of given number P
+
+            var q = new Queue<int>();
+            int N = 3;
+            int P = 16;
+
+            q.Enqueue(N);
+
+            int index = 0;
+
+            while (true)
+            {
+                var currentNumber = q.Dequeue();
+
+                if (currentNumber == P)
+                {
+                    Console.WriteLine($"Current number with index {index} is {P}");
+                    break;
+                }
+
+                q.Enqueue(currentNumber + 1);
+                q.Enqueue(currentNumber * 2);
+                index++;
+            }
+        }
+    }
+}
